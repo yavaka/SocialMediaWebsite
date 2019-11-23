022ee1fcc3e7a34f534920bdf48ff74c804b3425
yavaka <yavakaa@gmail.com>
2019-11-23 00:46:15 +0000

    Add Group model

---
+++ SocialMedia/SocialMedia.Models/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Models
+{
+    public class Group
+    {
+        public Group()
+        {
+            this.Members = new List<User>();
+            this.Posts = new List<Post>();
+        }
+
+        public int GroupId{ get; set; }
+        public string Title{ get; set; }
+        public string Description { get; set; }
+        public List<User> Members { get; set; }
+        public List<Post> Posts{ get; set; }
+    }
+}
