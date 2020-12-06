4cd65230c8f310c946280ad6928db5d54290c40c
yavaka <yavakasolutions@gmail.com>
2020-12-06 18:42:48 +0000

    Add ImageInputModel

---
+++ SocialMedia/SocialMedia.Web/Models/ImageInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialMedia.Web.Models
+{
+    using System.IO;
+
+    public class ImageInputModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Stream Content { get; set; }
+    }
+}
