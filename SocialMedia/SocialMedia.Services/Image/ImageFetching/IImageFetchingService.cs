bfd361464fe873c979b06d08e885759b862d9a99
yavaka <yavakasolutions@gmail.com>
2020-12-07 13:21:13 +0000

    Add ImageFetchingService

---
+++ SocialMedia/SocialMedia.Services/Image/ImageFetching/IImageFetchingService.cs
@@ -0,0 +1,16 @@
+﻿namespace SocialMedia.Services.Image.ImageFetching
+{
+    using SocialMedia.Services.Common;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    public interface IImageFetchingService : IService
+    {
+        Task<List<string>> GetAllImagesByUserId(string userId);
+
+        Task<Stream> GetThumbnail(string id);
+
+        Task<Stream> GetFullscreen(string id);
+    }
+}
---
+++ SocialMedia/SocialMedia.Services/Image/ImageFetching/ImageFetchingService.cs
@@ -0,0 +1,60 @@
+﻿namespace SocialMedia.Services.Image.ImageFetching
+{
+    using Microsoft.EntityFrameworkCore;
+    using SocialMedia.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.IO;
+    using Microsoft.Data.SqlClient;
+
+    public class ImageFetchingService : IImageFetchingService
+    {
+        private readonly SocialMediaDbContext _data;
+
+        public ImageFetchingService(SocialMediaDbContext data) => this._data = data;
+
+        public Task<List<string>> GetAllImagesByUserId(string userId)
+        => this._data
+            .ImagesData
+            .Where(u => u.UploaderId == userId)
+            .Select(i => i.Id.ToString())
+            .ToListAsync();
+
+        public Task<Stream> GetThumbnail(string id)
+        => this.GetImageData(id, "Thumbnail");
+
+        public Task<Stream> GetFullscreen(string id)
+        => this.GetImageData(id, "Fullscreen");
+
+        private async Task<Stream> GetImageData(string id, string size)
+        {
+            var db = this._data.Database;
+
+            var dbConnection = (SqlConnection)db.GetDbConnection();
+
+            var command = new SqlCommand(
+                $"SELECT {size}Content " +
+                $"FROM ImagesData " +
+                $"WHERE Id = @id;",
+                dbConnection);
+
+            command.Parameters.Add(new SqlParameter("@id", id));
+
+            dbConnection.Open();
+
+            var reader = await command.ExecuteReaderAsync();
+            Stream result = null;
+
+            if (reader.HasRows)
+            {
+                while (reader.Read()) result = reader.GetStream(0);
+            }
+
+            reader.Close();
+
+            return result;
+        }
+    }
+}
